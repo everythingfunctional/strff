53ee3d8232942e3d3b7588224f96394cbdc1af71
Brad Richardson <brichardson@structint.com>
2019-08-22 11:06:34 -0700

    Finish splitAt implementation

--- src/strff.f90
+++ src/strff.f90
@@ -4,14 +4,58 @@ module strff
 
     public :: splitAt
 contains
-    pure function splitAt(string, split_characters) result(strings)
-        use ISO_VARYING_STRING, only: VARYING_STRING, VAR_STR
+    pure recursive function splitAt(string, split_characters) result(strings)
+        use ISO_VARYING_STRING, only: VARYING_STRING, assignment(=)
 
         character(len=*), intent(in) :: string
         character(len=*), intent(in) :: split_characters
         type(VARYING_STRING), allocatable :: strings(:)
 
-        associate(a => split_characters); end associate
-        allocate(strings, source = [VAR_STR(string)])
+        integer :: string_length
+
+        if (len(split_characters) > 0) then
+            string_length = len(string)
+            if (string_length > 0) then
+                if (index(split_characters, string(1:1)) > 0) then
+                    allocate(strings, source = splitAt(string(2:), split_characters))
+                else if (index(split_characters, string(string_length:string_length)) > 0) then
+                    allocate(strings, source = splitAt(string(1:string_length - 1), split_characters))
+                else
+                    allocate(strings, source = doSplit(string, split_characters))
+                end if
+            else
+                allocate(strings(1))
+                strings(1) = ""
+            end if
+        else
+            allocate(strings(1))
+            strings(1) = string
+        end if
+    contains
+        pure function doSplit(string_, split_characters_) result(strings_)
+            character(len=*), intent(in) :: string_
+            character(len=*), intent(in) :: split_characters_
+            type(VARYING_STRING), allocatable :: strings_(:)
+
+            integer :: i
+            type(VARYING_STRING), allocatable :: rest(:)
+            integer :: string_length_
+            type(VARYING_STRING) :: this_string
+
+            string_length_ = len(string_)
+            do i = 2, string_length_
+                if (index(split_characters_, string_(i:i)) > 0) exit
+            end do
+            if (i < string_length_) then
+                this_string = string_(1:i - 1)
+                allocate(rest, source = splitAt(string_(i + 1:), split_characters_))
+                allocate(strings_(size(rest) + 1))
+                strings_(1) = this_string
+                strings_(2:) = rest(:)
+            else
+                allocate(strings_(1))
+                strings_(1) = string_
+            end if
+        end function doSplit
     end function splitAt
 end module strff
--- tests/strff_test.f90
+++ tests/strff_test.f90
@@ -9,11 +9,31 @@ contains
 
         type(TestItem_t) :: tests
 
-        type(TestItem_t) :: individual_tests(1)
+        type(TestItem_t) :: individual_tests(8)
 
         individual_tests(1) = it( &
                 "returns the same string when split on a character it doesn't contain", &
                 checkSplitDoesntContain)
+        individual_tests(2) = it( &
+                "can split strings at something", checkSplitAtSomething)
+        individual_tests(3) = it( &
+                "doesn't include an empty string at the end", &
+                checkNoEmptyEnd)
+        individual_tests(4) = it( &
+                "doesn't include an empty string at the beginning", &
+                checkNoEmptyBegin)
+        individual_tests(5) = it( &
+                "returns the same string when given no split characters", &
+                checkNoSplitCharacters)
+        individual_tests(6) = it( &
+                "doesn't include an empty string between split characters", &
+                checkNoEmptyBetween)
+        individual_tests(7) = it( &
+                "returns an empty string when given an empty string", &
+                checkForEmptyString)
+        individual_tests(8) = it( &
+                "returns an empty string when given a string that only contains split characters", &
+                checkForOnlySplitCharacters)
         tests = describe("splitAt", individual_tests)
     end function test_splitAt
 
@@ -32,4 +52,113 @@ contains
                 assertEquals(1, size(strings)) &
                 .and.assertEquals("Hello World", strings(1))
     end function checkSplitDoesntContain
+
+    pure function checkSplitAtSomething() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt("Hello,World", ","))
+        result_ = &
+                assertEquals(2, size(strings)) &
+                .and.assertEquals("Hello", strings(1)) &
+                .and.assertEquals("World", strings(2))
+    end function checkSplitAtSomething
+
+    pure function checkNoEmptyEnd() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt("Hello,World,", ","))
+        result_ = &
+                assertEquals(2, size(strings)) &
+                .and.assertEquals("Hello", strings(1)) &
+                .and.assertEquals("World", strings(2))
+    end function checkNoEmptyEnd
+
+    pure function checkNoEmptyBegin() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt(",Hello,World", ","))
+        result_ = &
+                assertEquals(2, size(strings)) &
+                .and.assertEquals("Hello", strings(1)) &
+                .and.assertEquals("World", strings(2))
+    end function checkNoEmptyBegin
+
+    pure function checkNoSplitCharacters() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt("Hello,World", ""))
+        result_ = &
+                assertEquals(1, size(strings)) &
+                .and.assertEquals("Hello,World", strings(1))
+    end function checkNoSplitCharacters
+
+    pure function checkNoEmptyBetween() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt("Hello, World", " ,"))
+        result_ = &
+                assertEquals(2, size(strings)) &
+                .and.assertEquals("Hello", strings(1)) &
+                .and.assertEquals("World", strings(2))
+    end function checkNoEmptyBetween
+
+    pure function checkForEmptyString() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEmpty, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt("", " ,"))
+        result_ = &
+                assertEquals(1, size(strings)) &
+                .and.assertEmpty(strings(1))
+    end function checkForEmptyString
+
+    pure function checkForOnlySplitCharacters() result(result_)
+        use ISO_VARYING_STRING, only: VARYING_STRING
+        use strff, only: splitAt
+        use Vegetables_m, only: Result_t, assertEmpty, assertEquals
+
+        type(Result_t) :: result_
+
+        type(VARYING_STRING), allocatable :: strings(:)
+
+        allocate(strings, source = splitAt(", ", " ,"))
+        result_ = &
+                assertEquals(1, size(strings)) &
+                .and.assertEmpty(strings(1))
+    end function checkForOnlySplitCharacters
 end module strff_test
